98c4c651f4350088037d97857603bd09aa5554a0
devarshi007 <devarshi.trivedi@improwised.com>
2023-04-10 19:50:04 +0530

    challang completed

---
+++ script.js
@@ -0,0 +1,44 @@
+//delay
+async function getDelay(){
+    const response = await fetch("https://httpbin.org/delay/10");
+    const delay = await response.json();
+}
+
+//uuid
+async function getUuid(){
+    const response = await fetch("https://httpbin.org/uuid");
+    const uuid = await response.json();
+    return uuid.uuid;
+}
+
+//anything
+async function anything(uuid){
+    const response = await fetch(`https://httpbin.org/anything`,{method:'POST',body:uuid});
+    const anything = await response.json();
+    return anything;
+}
+
+async function total(){
+    let startTime = Date.now();
+    getDelay();
+    let myPromise = getUuid();
+    
+    myPromise.then(
+        function(value){
+            return value
+        }
+    ).then(
+        function(value){
+            let anyPromise = anything(value);
+            anyPromise.then(
+                function(value){
+                    let endTime = Date.now();
+                    console.log(("Time: "+(endTime-startTime)/1000));
+                    console.log(value.data);
+                }
+            )
+        }
+    )
+}
+
+total();
